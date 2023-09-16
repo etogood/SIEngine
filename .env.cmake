36938c46c0fc166902ecc28d3e4efb08b2d4ec95
etogood <ilya.etogood@yandex.ru>
2023-09-16 18:55:24 +0300

    first window

---
+++ .env.cmake
@@ -0,0 +1,6 @@
+set(GLFW_PATH C:/dev/Libraries/glfw-3.3.8.bin.WIN64)
+set(GLM_PATH C:/dev/Libraries/glm)
+set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.261.1)
+
+# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
+set(MINGW_PATH "C:/dev/msys2")
