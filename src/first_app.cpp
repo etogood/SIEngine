2bfb791157332f4130f4d7debed3991f6dfd7863
etogood <ilya.etogood@yandex.ru>
2023-10-07 23:14:50 +0300

    Push Constants

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -4,10 +4,22 @@
 
 #include "first_app.hpp"
 
+// Libs
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 // std
 #include <array>
 
 namespace si {
+
+    // temp
+    struct SimplePushConstantData {
+        glm::vec2 offset;
+        alignas(16) glm::vec3 color;
+    };
+
     FirstApp::FirstApp() {
         loadModels();
         createPipelineLayout();
@@ -38,12 +50,18 @@ namespace si {
     }
 
     void FirstApp::createPipelineLayout() {
+
+        VkPushConstantRange pushConstantRange{};
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(SimplePushConstantData);
+
         VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount = 0;
-        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 
         if (vkCreatePipelineLayout(siDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
             throw std::runtime_error("failed to create pipeline layout");
@@ -108,6 +126,9 @@ namespace si {
     }
 
     void FirstApp::recordCommandBuffer(int imageIndex) {
+        static int frame = 0;
+        frame = (frame + 1) % 100;
+
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -124,7 +145,7 @@ namespace si {
         renderPassInfo.renderArea.extent = siSwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
         clearValues[1].depthStencil = {1.0f, 0U};
 
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
@@ -146,7 +167,18 @@ namespace si {
 
         siPipeline->bind(commandBuffers[imageIndex]);
         siModel->bind(commandBuffers[imageIndex]);
-        siModel->draw(commandBuffers[imageIndex]);
+
+        for (int j = 0; j < 4; j++) {
+            SimplePushConstantData push{};
+            push.offset = {-0.5f + frame * 0.02f, -0.4f + j * 0.25f};
+            push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
+
+            vkCmdPushConstants(commandBuffers[imageIndex], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
+            siModel->draw(commandBuffers[imageIndex]);
+
+        }
+        
+        
 
         vkCmdEndRenderPass(commandBuffers[imageIndex]);
         if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS){
@@ -173,7 +205,7 @@ namespace si {
             recreateSwapChain();
             return;
         }
-        if (result != VK_SUCCESS){
+        if (result != VK_SUCCESS) {
             throw std::runtime_error("failed to present swap chain image!");
         }
     }
